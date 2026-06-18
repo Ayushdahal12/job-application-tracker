a57ce249605048593acabdc799484c775740c412
Ayushdahal12 <138177510+Ayushdahal12@users.noreply.github.com>
2026-06-18 09:31:47 +0545

    fix: force redeploy cors

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import applicationRoutes from "./routes/applications.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors({
